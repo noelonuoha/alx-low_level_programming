bb186247572bd2ec3bd7274b05cd1ef231b8a465
noelonuoha <chukzydalegendary27@gmail.com>
2022-12-10 06:50:16 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabets
+ *
+ * Return: 0 alway
+ */
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0, i < 26; i++)
+{
+	putchar(alp[1])
+}
+putchar('\n');
+return (0);
+}
